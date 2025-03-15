0757304e4753f4d4a1dc2da333319c5a25faac83
GURUPRANESH-M <gurupraneshkcs@gmail.com>
2025-03-15 11:55:24 +0530

    resolved registration issue

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["admin", "staff"], default: "staff" } // Default is staff
+  role: { type: String, enum: ["admin", "staff", "student"], default: "student" }, // Added student to the enum
+  // Remove or make name optional
+  // name: { type: String, required: true }, //remove this line
+  name: {type: String}, // or make it optional
 });
 
 module.exports = mongoose.model("User", userSchema);
